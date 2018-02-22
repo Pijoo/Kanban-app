1081ad35f0c9790ef8399b36c8027d434ca44bcf
Pijo <surzyn_b@interia.pl>
2018-02-22 18:56:34 +0100

    Task Kanban

---
+++ server/controllers/lane.controller.js
@@ -0,0 +1,60 @@
+import uuid from 'uuid';
+import Lane from '../models/lane';
+
+
+export function addLane(req, res) {
+	if (!req.body.name) {
+		res.status(403).end();
+	}
+
+	const newLane = new Lane(req.body);
+
+	newLane.notes = [];
+
+	newLane.id = uuid();
+	newLane.save((err, saved) => {
+		if (err) {
+			res.status(500).send(err);
+		}
+		res.json(saved);
+	});
+}
+
+export function getLanes(req, res) {
+	Lane.find().exec((err, lanes) => {
+		if (err) {
+			res.status(500).send(err);
+		}
+		res.json({ lanes });
+	});
+}
+
+export function deleteLane(req, res) {
+	Lane.findOne({ id: req.params.laneId }).exec((err, lane) => {
+		if (err) {
+			res.status(500).send(err);
+		}
+
+		lane.notes.forEach((note) => {
+			note.remove();
+		});
+
+		lane.remove(() => {
+			res.status(200).end();
+		});
+	});
+}
+
+export function editLane(req, res) {
+	if (!req.body.name) {
+		res.status(400).end();
+	}
+
+	Lane.findOneAndUpdate({ id: req.params.laneId }, { name: req.body.name }, { new: true }, (updateErr, updated) => {
+		if (updateErr) {
+			res.status(500).send(updateErr);
+	}
+
+	res.json(updated);
+	});
+}
---
+++ server/controllers/note.controller.js
@@ -0,0 +1,77 @@
+import Note from '../models/note';
+import Lane from '../models/lane';
+import mongoose from 'mongoose';
+import uuid from 'uuid';
+
+export function addNote(req, res) {
+	const { note, laneId } = req.body;
+
+	if (!note || !note.task || !laneId) {
+		res.status(400).end();
+	}
+
+	const newNote = new Note({
+		task: note.task,
+	});
+
+	newNote.id = uuid();
+	newNote.save((err, saved) => {
+		if (err) {
+			res.status(500).send(err);
+		}
+		Lane.findOne({ id: laneId })
+			.then(lane => {
+			lane.notes.push(saved);
+			return lane.save();
+			})
+			.then(() => {
+			res.json(saved);
+			});
+	});
+}
+
+export function deleteNote(req, res) {
+	Note
+	.findOne({ id: req.params.noteId })
+	.exec((noteErr, note) => {
+		if (noteErr) {
+			res.status(500).send(noteErr);
+		}
+
+		Lane
+		.findOne({ notes: { $in: [mongoose.Types.ObjectId(note._id)] } })
+		.exec((laneErr, lane) => {
+			if (laneErr) {
+				res.status(500).send(laneErr);
+			}
+
+			lane
+			.update(
+				{ $pull: { notes: mongoose.Types.ObjectId(note._id) } },
+				(updateErr) => {
+					if (updateErr) {
+						res.status(500).send(updateErr);
+					}
+
+					note.remove(() => {
+						res.status(200).end();
+					});
+				}
+			);
+		});
+	});
+}
+
+export function editNote(req, res) {
+	if (!req.body.task) {
+		res.status(400).end();
+	}
+
+	Note
+	.findOneAndUpdate({ id: req.params.noteId }, { task: req.body.task }, { new: true }, (updateErr, updated) => {
+		if (updateErr) {
+			res.status(500).send(updateErr);
+		}
+		res.json(updated);
+	});
+}
