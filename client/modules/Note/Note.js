1aabfc0be20f723cea44b1299bc42a0112751f14
Pijo <surzyn_b@interia.pl>
2018-03-06 16:59:10 +0100

    DnD added

--- client/modules/Note/Note.js
+++ client/modules/Note/Note.js
@@ -1,29 +1,70 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-// Import Style
+import React, { PropTypes } from 'react';
 import styles from './Note.css';
+import ItemTypes from '../Kanban/itemTypes';
+import { DragSource, DropTarget } from 'react-dnd';
+import { compose } from 'redux';
 
-class Note extends Component {
-  render() {
-    return (
-    );
-  }
+class Note extends React.Component {
+	render() {
+		const {
+			connectDragSource,
+			connectDropTarget,
+			isDragging,
+			editing,
+			children,
+		} = this.props;
+
+		const dragSource = editing ? a => a : connectDragSource;
+
+		return dragSource(connectDropTarget(
+			<li
+				className={styles.Note}
+				style={{
+					opacity: isDragging ? 0 : 1,
+				}}
+			>
+				{children}
+			</li>
+	 ));
+	}
 }
 
-const mapStateToProps = (state) => {
-  return {};
+const noteSource = {
+	beginDrag(props) {
+		return {
+			id: props.id,
+			laneId: props.laneId,
+		};
+	},
+	isDragging(props, monitor) {
+		return props.id === monitor.getItem().id;
+	},
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {};
+const noteTarget = {
+	hover(targetProps, monitor) {
+		const sourceProps = monitor.getItem();
+
+		if (targetProps.id !== sourceProps.id) {
+			targetProps.moveWithinLane(targetProps.laneId, targetProps.id, sourceProps.id);
+		}
+	},
 };
 
 Note.propTypes = {
+	children: PropTypes.any,
+	connectDragSource: PropTypes.func,
+	connectDropTarget: PropTypes.func,
+	isDragging: PropTypes.bool,
+	editing: PropTypes.bool,
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+export default compose(
+	DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+		connectDragSource: connect.dragSource(),
+		isDragging: monitor.isDragging(),
+	})),
+	DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+		connectDropTarget: connect.dropTarget(),
+	}))
 )(Note);
